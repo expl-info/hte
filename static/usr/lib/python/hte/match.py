7dbff5c5a548eb0ce90d27a29f31480cddccfc02
John Marshall <johnm.dev@gmail.com>
2015-01-07 22:00:58 -0500

    Add docstrings for match functions

--- static/usr/lib/python/hte/match.py
+++ static/usr/lib/python/hte/match.py
@@ -28,22 +28,31 @@ class Matcher:
 		return self.matchfn(child, self.node)
 
 def matchany(child, node, **kwargs):
+	"""Match against text or element.
+	"""
 	if type(child) in StringTypes:
 		return matchtext(child, node)
 	elif isinstance(child, Elem):
 		return matchelem(child, node)
 
 def matchchildregexp(child, node, **kwargs):
+	"""Match against element with a child that matches against a
+	compiled regexp.
+	"""
 	return isinstance(child, Elem) \
 		and child.children \
 		and matchregexp(child.children[0], node)
 
 def matchchildtext(child, node, **kwargs):
+	"""Match against element with a child that matches against text.
+	"""
 	return isinstance(child, Elem) \
 		and child.children \
 		and matchtext(child.children[0], node)
 
 def matchelem(child, node, **kwargs):
+	"""Match against an element.
+	"""
 	if type(child) == type(node) \
 		and isinstance(child, Elem) \
 		and child.tag == node.tag \
@@ -55,10 +64,14 @@ def matchelem(child, node, **kwargs):
 	return False
 
 def matchregexp(child, node, **kwargs):
+	"""Match against a compiled regexp.
+	"""
 	return type(child) in StringTypes \
 		and node.match(child)
 
 def matchtext(child, node, **kwargs):
+	"""Match against text.
+	"""
 	return type(child) in StringTypes \
 		and type(node) in StringTypes \
 		and child == node
